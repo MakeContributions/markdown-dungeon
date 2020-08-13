3538f3facca3b6a4579b71c47f2566e41033d6da
Ming Tsai <ming.jia1213@gmail.com>
2020-08-12 20:14:57 -0400

    chore(404): change design

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import Layout from '../components/layout';
+import { Link } from 'gatsby';
 
-export default function () {
-  return <p>404</p>;
+export default function NotFound() {
+  return (
+    <Layout>
+      <h1>Page Not Found</h1>
+      <p>Oops, we couldn't find this page!</p>
+      <Link to='/'>Go back</Link>
+    </Layout>
+  );
 }
