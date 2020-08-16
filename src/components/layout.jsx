bb1a4d2e626d91c03c08af404a31d555e12bbe59
Ming Tsai <ming.jia1213@gmail.com>
2020-08-16 18:19:15 -0400

    style: add border and border radius

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 
 export default function Layout({ children }) {
   return (
-    <div style={{ margin: `0 auto`, maxWidth: 650, padding: `1rem 1rem` }}>
-      {children}
+    <div>
+      <br />
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 650,
+          padding: `1rem 1rem`,
+          border: `1px solid #e1e4e8`,
+          borderRadius: `10px`,
+        }}
+      >
+        {children}
+      </div>
+      <br />
     </div>
   );
 }
