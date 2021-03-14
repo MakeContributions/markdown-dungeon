aa05c6a10f1f75076cc3a087d43c14eb8a457d88
Ming Tsai <ming.jia1213@gmail.com>
2021-03-14 11:55:25 -0400

    chore: add dungeon info

---
+++ src/components/card-grid.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import infos from '../data/dungeon-info.json';
+import { homeStyles } from '../utils/styles';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+
+export default function CardGrid(props) {
+  const classes = homeStyles();
+  const post = props.post;
+  const slug = post.fields.slug;
+  const splited = slug.split('/');
+  const info = infos.find(
+    (d) => d.name === splited[2] && d.language === splited[1]
+  );
+  return (
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.cardMedia}
+        image='https://source.unsplash.com/random'
+        title='Image title'
+      />
+      <CardContent className={classes.cardContent}>
+        <Typography gutterBottom variant='h5' component='h2'>
+          {info.display}
+        </Typography>
+        <Typography>{post.excerpt}</Typography>
+      </CardContent>
+      <CardActions dir='rtl'>
+        <Button size='small' color='primary' href={slug.slice(0, -1)}>
+          <ArrowForwardIcon />
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,78 +1,25 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import Link from '@material-ui/core/Link';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Make Contributions
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
-const useStyles = makeStyles((theme) => ({
-  icon: {
-    marginRight: theme.spacing(2),
-  },
-  heroContent: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6),
-  },
-  heroButtons: {
-    marginTop: theme.spacing(4),
-  },
-  cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
-  },
-  card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  cardMedia: {
-    paddingTop: '56.25%', // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
-  },
-}));
-
-function getName(url) {
-  return url.split('/')[2];
-}
+import Copyright from '../components/copyright';
+import CardGrid from '../components/card-grid';
+import { homeStyles } from '../utils/styles';
 
 export default function Home({ data }) {
-  const classes = useStyles();
+  const classes = homeStyles();
   const posts = data.allMarkdownRemark.nodes;
 
   return (
     <React.Fragment>
       <CssBaseline />
-      <AppBar position="relative">
+      <AppBar position='relative'>
         <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap>
+          <Typography variant='h6' color='inherit' noWrap>
             Markdown Dungeon
           </Typography>
         </Toolbar>
@@ -80,40 +27,33 @@ export default function Home({ data }) {
       <main>
         {/* Hero unit */}
         <div className={classes.heroContent}>
-          <Container maxWidth="sm">
-            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+          <Container maxWidth='sm'>
+            <Typography
+              component='h1'
+              variant='h2'
+              align='center'
+              color='textPrimary'
+              gutterBottom
+            >
               Markdown Dungeon
             </Typography>
-            <Typography variant="h5" align="center" color="textSecondary" paragraph>
-              A dungeon is a room or cell in which prisoners are held, especially underground.
+            <Typography
+              variant='h5'
+              align='center'
+              color='textSecondary'
+              paragraph
+            >
+              A dungeon is a room or cell in which prisoners are held,
+              especially underground.
             </Typography>
           </Container>
         </div>
-        <Container className={classes.cardGrid} maxWidth="md">
+        <Container className={classes.cardGrid} maxWidth='md'>
           {/* End hero unit */}
           <Grid container spacing={4}>
             {posts.map((post, index) => (
               <Grid item key={index} xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      {getName(post.fields.slug)}
-                    </Typography>
-                    <Typography>
-                      {post.excerpt}
-                    </Typography>
-                  </CardContent>
-                  <CardActions dir="rtl">
-                    <Button size="small" color="primary" href={post.fields.slug.slice(0, -1)}>
-                      View
-                    </Button>
-                  </CardActions>
-                </Card>
+                <CardGrid post={post} />
               </Grid>
             ))}
           </Grid>
@@ -130,7 +70,9 @@ export default function Home({ data }) {
 
 export const query = graphql`
   query {
-    allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/begin-journey/"}}) {
+    allMarkdownRemark(
+      filter: { fileAbsolutePath: { regex: "/begin-journey/" } }
+    ) {
       nodes {
         fields {
           slug
