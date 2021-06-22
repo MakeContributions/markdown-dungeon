c73378c452873786ba6f9f94c1f7461a2345a13d
Akshat Jindal <Akshat.Jindal@honeywell.com>
2021-06-22 20:53:20 +0530

    Added link to home page on rooms

--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import { Home } from '@material-ui/icons';
+import Link from '@material-ui/core/Link';
 import Layout from '../components/layout';
 
 // highlight-start
@@ -15,6 +17,8 @@ export default function BlogPost({ data }) {
   // highlight-end
   return (
     <Layout>
+      <Link href="#" onClick={() => window.location.href = '/'} color="primary"><Home /></Link>
+      <br />
       <div dangerouslySetInnerHTML={{ __html: html }} />
     </Layout>
   );
