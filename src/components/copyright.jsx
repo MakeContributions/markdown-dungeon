b7e621e04fac17ac7a38dba5ae0ce05492239505
Akshit Arora <akshitarora9211@gmail.com>
2021-09-26 20:19:20 +0530

    fix: footer reference (#196)

--- src/components/copyright.jsx
+++ src/components/copyright.jsx
@@ -6,7 +6,7 @@ export default function Copyright() {
   return (
     <Typography variant='body2' color='textSecondary' align='center'>
       {'Copyright © '}
-      <Link color='inherit' href='https://material-ui.com/'>
+      <Link color='inherit' href='https://github.com/MakeContributions'>
         Make Contributions
       </Link>{' '}
       {new Date().getFullYear()}
