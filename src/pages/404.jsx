cf240b47710d59a6054f9722ce9b1ef1738638f3
Gem <97372545+disabledandfab@users.noreply.github.com>
2022-06-16 08:22:00 -0500

    chore: change not found sentence (#222)

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import Layout from '../components/layout';
 import { Link } from 'gatsby';
-
 export default function NotFound() {
   return (
     <Layout>
       <h1>Page Not Found</h1>
-      <p>Oops, we couldn't find this page!</p>
+      <p>Oops! A monster seems to have eaten this page. Or maybe you fell down the wrong wormhole?!</p>
       <Link to='/'>Go back</Link>
     </Layout>
   );
